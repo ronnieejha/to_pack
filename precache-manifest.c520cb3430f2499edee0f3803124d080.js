ed2ae6a0e4b6bc77598a75ba8ddbd71e5f7b933a
Ashish Jha <ashish.jha2@oyorooms.com>
2020-06-07 19:18:54 +0530

    Updates

---
+++ precache-manifest.c520cb3430f2499edee0f3803124d080.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f14af6cbd55a13079b29217f0040345f",
+    "url": "/to_pack/index.html"
+  },
+  {
+    "revision": "082205c304407d93a1ad",
+    "url": "/to_pack/static/css/main.d2d0d2de.chunk.css"
+  },
+  {
+    "revision": "addedd7fa4fe6b9bc2cc",
+    "url": "/to_pack/static/js/2.27c8417e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/to_pack/static/js/2.27c8417e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "082205c304407d93a1ad",
+    "url": "/to_pack/static/js/main.511ce7d8.chunk.js"
+  },
+  {
+    "revision": "01c15d8ce652e911f8e6",
+    "url": "/to_pack/static/js/runtime-main.c2e090aa.js"
+  }
+]);
